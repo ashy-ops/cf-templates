f8ed3279f5a4614e14e274d78ffe3ea798574f10
ashy-ops <ashmitsinghsaggu2007@gmail.com>
2025-10-29 11:02:50 +0530

    Update KEY_THINGS.m

--- KEY_THINGS.m
+++ KEY_THINGS.m
@@ -6,4 +6,6 @@
 1.) Binary Search,DFS,BFS,some pattern?,sortings,recurssion,bitmasks,brute force?
 
 KEEP THIS VIDEO IN MIND-https://www.youtube.com/watch?v=WTr12dK2Se0
-Treat the problems creatively!
+#Treat the problems creatively!
+#Check the Parameters Size and things like sum of numbers over all test case should be less than this and then look at the time limit and then think of how complex you can make it?
+maybe you can then brute force it? or maybe pherhaps O(n^2)?
