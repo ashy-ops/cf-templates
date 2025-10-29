4404ab374f044b68398b17f08bca387d48f94dbd
ashy-ops <ashmitsinghsaggu2007@gmail.com>
2025-10-29 10:59:02 +0530

    Update KEY_THINGS.m

--- KEY_THINGS.m
+++ KEY_THINGS.m
@@ -2,3 +2,8 @@
 #Look for patterns of numbers.
 #if division involved make sure always that the divisor can never be 0 otherwise run time error throws.
 #Make sure to think a little more creatively than being rigid for the problem.
+#Try to think for the following thins-
+1.) Binary Search,DFS,BFS,some pattern?,sortings,recurssion,bitmasks,brute force?
+
+KEEP THIS VIDEO IN MIND-https://www.youtube.com/watch?v=WTr12dK2Se0
+Treat the problems creatively!
